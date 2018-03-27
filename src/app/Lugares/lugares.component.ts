c343ccd5d32ad1008f0433df5a37e81f875e641e
ernestognw <ernestognw@gmail.com>
2018-03-27 12:12:36 -0600

    Clase 29

--- src/app/Lugares/lugares.component.ts
+++ src/app/Lugares/lugares.component.ts
@@ -8,9 +8,9 @@ export class LugaresComponent {
 	title = 'PlatziSquare';
 	lugares:any = [
 	{id: 1, plan: 'pagado', cercania: 1, distancia: 1, active: 1, nombre: 'Florería la Gardenia'},
-	{id: 2, plan: 'gratuito', cercania: 1, distancia: 1.8, active: 0, nombre: 'Donas la Pasadita'},
+	{id: 2, plan: 'gratuito', cercania: 1, distancia: 1.8, active: 1, nombre: 'Donas la Pasadita'},
 	{id: 3, plan: 'gratuito', cercania: 2, distancia: 5, active: 1, nombre: 'Veterinaria Huellitas'},
-	{id: 4, plan: 'gratuito', cercania: 3, distancia: 10, active: 0, nombre: 'Sushi Sushiroll'},
+	{id: 4, plan: 'gratuito', cercania: 3, distancia: 10, active: 1, nombre: 'Sushi Sushiroll'},
 	{id: 5, plan: 'pagado', cercania: 3, distancia: 35, active: 1, nombre: 'Hotel la Gracia'},
 	{id: 6, plan: 'gratuito', cercania: 3, distancia: 120, active: 1, nombre: 'Zapatería el clavo'},
 	];
